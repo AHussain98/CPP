cb710228f2f1b48969f506b79df7313929f34925
AHussain98 <106779657+AHussain98@users.noreply.github.com>
2023-03-13 19:02:37 +0000

    Add files via upload

--- Hashing.cpp
+++ Hashing.cpp
@@ -4,6 +4,7 @@
 # include <array>
 # include <algorithm>
 # include <unordered_map>
+# include <map>
 
 // associative containers store their elements in an order which depends on teh key, std::set, std::map etc... use a tree
 // unsorted associative containers use a hash table
@@ -73,5 +74,27 @@ map.insert({"Asad", 24});
 map.insert({"Misha", 10});
 
 // we can have duplicates if we use unordered_multimap
+
+// c++11 provides unordered versions of all the associative containers, unordered_set, multiset, map and multimap
+// unsorted containers sypport typical associative container operations such as insert(), find() and erase()
+// operations on an unsorted container are usually faster than they are on a sorted container, especially for large containers with many insert() and erase() operations
+// however, they can be considerably slower if the hash table needs to be resized, or if there are many collisions, in time critical applications this delay may not be acceptable
+
+// unordered containers only allow forward iteration, rbegin()/rend() and crbegin()/crend() are not supported
+// an iterator to an unordered container cannot be decremented
+// as the containers have no concept of order, iterator ranges are not particularly useful, expect for iterating over the entire container (forward only) or iterating over elements with the same key (multiset/multimap)
+
+// unordered versions of multiset and multimap do not support lower_bound() and upper_bound(), we can still use equal_range() and find() and count()
+
+// sometimes we need to have the elements sorted but using a sorted container would add too much overhead
+// in this case, we can use an unsorted container and copy its elements into a sorted container when we've finished
+
+std::map<std::string,int> sorted_map;
+
+std::copy(cbegin(map),cend(map), inserter(sorted_map, end(sorted_map)));
+
+for (auto i : sorted_map){
+    std::cout << i.first << " "; // sorted
+}
 
 }
