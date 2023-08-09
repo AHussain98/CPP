bd0c951263f01424355c666f7dbcb5cef245c52b
AHussain98 <106779657+AHussain98@users.noreply.github.com>
2023-08-09 12:36:19 +0100

    Update Notes.cpp

--- Notes.cpp
+++ Notes.cpp
@@ -540,4 +540,31 @@ extern: The extern storage class is used to give a reference of a global variabl
 mutable: The mutable specifier applies only to class objects, which are discussed later in this tutorial. It allows a member of an object to override const member function. That is, a mutable member can be modified by a const member function.
 
 
+Cohesion refers to what the class (or module) can do. Low cohesion would mean that the class does a great variety of actions - it is broad, unfocused on what it should do. High cohesion means that the class is focused on what it should be doing, i.e. only methods relating to the intention of the class.
 
+Example of Low Cohesion:
+
+-------------------
+| Staff           |
+-------------------
+| checkEmail()    |
+| sendEmail()     |
+| emailValidate() |
+| PrintLetter()   |
+-------------------
+Example of High Cohesion:
+
+----------------------------
+| Staff                   |
+----------------------------
+| -salary                 |
+| -emailAddr              |
+----------------------------
+| setSalary(newSalary)    |
+| getSalary()             |
+| setEmailAddr(newEmail)  |
+| getEmailAddr()          |
+----------------------------
+As for coupling, it refers to how related or dependent two classes/modules are toward each other. For low coupled classes, changing something major in one class should not affect the other. High coupling would make it difficult to change and maintain your code; since classes are closely knit together, making a change could require an entire system revamp.
+
+Good software design has high cohesion and low coupling.
