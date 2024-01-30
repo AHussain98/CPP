d7075948940e0fd21f57f1174d194c2572b26d42
AHussain98 <106779657+AHussain98@users.noreply.github.com>
2024-01-30 23:00:02 +0000

    Add files via upload

--- Books/FixedIncome.cpp
+++ Books/FixedIncome.cpp
@@ -1,6 +1,7 @@
 # include <iostream>
 # include <vector>
 # include <cmath>
+# include <string>
 
 // lets calculate a simple interest rate
 // interest rates determine how much a financial institution will pay in exchange for holding a cash deposit over a period of time
@@ -113,6 +114,135 @@ double CompoundIntRateCalculator::continousCompounding(double value, int numPeri
 }
 
 // Modelling cash flows
+// a cash flow is a sequence of payments, scheduled during a specified period of time between two parties
+// the value of the cash flows should be equivalent in some way
+// cash flows are a basic tool for comparing fixed income investments
+// the present value of a payment in the future needs to be discounted by the interest rate that would be applied to that same value
+// discounting is the inverse concept to compounding
+
+// present value of a future payment is PV = FV / (1 + R)^N
+// PV is the desired present value, FV is the future value that we want to discount
+// R is interest rate and N is number of periods
+
+class CashFlowCalculator {
+public:
+	CashFlowCalculator(double rate);
+	CashFlowCalculator(const CashFlowCalculator& v);
+	CashFlowCalculator& operator= (const CashFlowCalculator& v);
+	~CashFlowCalculator();
+	void addCashPayment(double value, int timePeriod);  // add new payments to the cash flow
+	double presentValue();
+private:
+	std::vector<double> m_cashPayments;
+	std::vector<int> m_timePeriods;
+	double m_rate;
+	double presentValue(double futureValue, int timePeriod);  // used to compute the PV or the whole cash flow stored in the current object
+};
+
+CashFlowCalculator::CashFlowCalculator(double rate) : m_rate(rate)
+{
+
+}
+
+CashFlowCalculator::CashFlowCalculator(const CashFlowCalculator& v) : m_rate(v.m_rate)
+{
+
+}
+
+CashFlowCalculator::~CashFlowCalculator()
+{
+
+}
+
+CashFlowCalculator& CashFlowCalculator::operator=(const CashFlowCalculator& v)
+{
+	if (this != &v)
+	{
+		this->m_cashPayments = v.m_cashPayments;
+		this->m_rate = v.m_rate;
+		this->m_timePeriods = v.m_timePeriods;
+	}
+	return *this;
+}
+
+
+double CashFlowCalculator::presentValue() 
+{
+	double total = 0;
+	for (int i = 0; i < m_cashPayments.size(); ++i)
+	{
+		total += presentValue(m_cashPayments[i], m_timePeriods[i]);
+	}
+	return total;
+}
+
+void CashFlowCalculator::addCashPayment(double value, int timePeriod)
+{
+	m_cashPayments.push_back(value);
+	m_timePeriods.push_back(timePeriod);
+}
+
+
+double CashFlowCalculator::presentValue(double futureValue, int timeperiod)
+{
+	// function just for calculating the PV for a single payment
+	double pValue = futureValue / std::pow(1 + m_rate, timeperiod);
+	std::cout << " value " << pValue << std::endl;
+	return pValue;
+}
+
+// Bonds are a very common fixed income instrument
+// bonds are structured in a way that the investor deposits the principal value at the beginning of the bend
+// this principal is often repaid at expiry
+// between the start period and expiry, investors are paid a constant value, called the coupon value, which determines the interest rate paid on the bond
+
+class BondCalculator {
+public:
+	BondCalculator(const std::string institution, int numPeriods, double principal, double couponValue);
+	BondCalculator(const BondCalculator& v);
+	BondCalculator& operator= (const BondCalculator& v);
+	~BondCalculator();
+
+	double interestRate();  // used to return the internal rate of return implied by the coupon
+
+private:
+	std::string m_institution;
+	double m_principal;
+	double m_coupon;
+	int m_numPeriods;
+};
+
+BondCalculator::BondCalculator(const std::string institution, int numPeriods, double principal, double couponValue)
+{
+
+}
+
+BondCalculator::BondCalculator(const BondCalculator& v) : m_institution(v.m_institution), m_numPeriods(v.m_numPeriods), m_principal(v.m_principal), m_coupon(v.m_coupon)
+{
+
+}
+
+BondCalculator::~BondCalculator()
+{
+}
+
+
+BondCalculator& BondCalculator::operator =(const BondCalculator& v)
+{
+	if (this != &v)  // check they're not the same object
+	{
+		this->m_institution = v.m_institution;
+		this->m_principal = v.m_principal;
+		this->m_numPeriods = v.m_numPeriods;
+		this->m_coupon = v.m_coupon;
+	}
+	return *this; // return by this object by reference
+}
+
+double BondCalculator::interestRate()
+{
+	return m_coupon / m_principal;
+}
 
 
 int main() {
@@ -129,4 +259,8 @@ int main() {
 	CompoundIntRateCalculator comp_1(compound_rate);
 	std::cout << comp_1.multiplePeriod(compound_value, num_periods) << std::endl;
 	std::cout << comp_1.continousCompounding(compound_value, num_periods) << std::endl;
+
+
+
+
 }
