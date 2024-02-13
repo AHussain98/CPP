d04d964c0afe27057e639f227ab62142a8e09d2e
AHussain98 <106779657+AHussain98@users.noreply.github.com>
2024-02-13 21:37:14 +0000

    Add files via upload

--- Books/CPP_for_Finance.cpp
+++ Books/CPP_for_Finance.cpp
@@ -7,6 +7,7 @@
 //# include "boost/date_time/gregorian/gregorian.hpp"
 # include "boost/ratio/ratio.hpp"
 //using namespace boost::gregorian;
+# include <string>
 using namespace boost;
 
 
@@ -542,6 +543,264 @@ void printRatios()
 	std::cout << "one_third numerator: " << one_third.num
 		<< " denominator: " << one_third.den;
 }
+
+// encapsulation means hiding data, which becomes the member varoables of the target class
+// consider a credit default swap
+enum CDSUnderlying {
+	CDSUnderlyingBond,
+	CDSUnderlyingCash
+};
+
+class CDSContract {
+public:
+	CDSContract();
+	CDSContract(const CDSContract& p);
+	~CDSContract();
+	CDSContract& operator= (const CDSContract& p);
+	std::string counterpart() { return m_counterpart; }
+	void setCounterpart(const std::string& s); //{ m_counterpart = s; }  // any changes happening to m_counterpart and m_payoff will only occur through an operation on CDSContract
+	double payoff() { return m_payoff; }
+	void setPayoff(double payoff) { m_payoff = payoff; }
+	virtual double computeCurrentValue(const Date& d);
+
+private:
+	std::string m_counterpart;
+	CDSUnderlying m_underlying;
+	double m_payoff;
+	int m_term;
+	double m_spreadCost;
+
+	const static double kStandardPayoff; // made static because its the same for all members of the class
+};
+
+void CDSContract::setCounterpart(const std::string& s)
+{
+	m_counterpart = s;
+	setPayoff(kStandardPayoff);
+}
+
+class LoanOnlyCDSContract : public CDSContract
+{
+public:
+	void changeLoanSource(const std::string& s);
+	virtual double computeCurrentValue(const Date& d);
+private:
+	std::string m_loanSource;
+};
+
+// Whenever the counterpart for the contract changes, the class reacts by resetting the 
+// payoff to a standard value(defined by the constant kStandardPayoff).That would not
+// be possible if the m_counterpart data member were not properly encapsulated into the CDSContract class
+
+/* Inheritance requires that the new class be used in a context similar to the 
+original class. Therefore, inheritance shouldn’t be used to create classes that have 
+just a superficial similarity to the original class. In particular, a class that inherits 
+from a base class could be used in the same code as the original class. If this is 
+not true for the new class you need, it is better to create a separate class with a 
+specialized interface.*/
+
+// you can pass a derived class to a function that expects an object of the base class
+
+/* 
+The first step in using polymorphism via virtual functions is to understand how they
+differ from regular member functions. When a virtual function is called, the compiler
+has to determine the type of call and translate it into binary code that will perform the
+call to the correct implementation. This is done in C++ using the so-called virtual table
+mechanism.
+A virtual table is a vector of functions that is created for each class that uses at least
+one virtual function. The virtual table stores the addresses of virtual functions that have
+been declared for that particular type */
+
+/* Although creating class hierarchies is a useful technique for code maintenance and 
+sharing, inheritance may not be the best strategy for code organization in some cases. It 
+is important to be able to identify the situations in which other approaches would work 
+better. Here are some potential disadvantages of using inheritance:
+• Increased coupling between classes: Once you decide to use 
+inheritance, there is a big interdependence between classes. A small 
+change in the base class can affect all descendent classes. If there is 
+a situation where the base class can vary frequently in functionality 
+and responsibilities, then inheritance may not be the best solution.
+• Physical dependencies at compilation time: In C++, inheritance also 
+creates a compile-time dependency between classes. To generate 
+correct code, the C++ compiler needs to access the definition of each 
+base class. This may result in increased compilation time, which is 
+sometimes undesirable, especially in large software projects.
+• Increased information coupling: Class hierarchies may also require 
+developers to learn the multiple implementations of different classes 
+at different levels. This is necessary especially when classes are not 
+well designed and information about their operations is not clear.*/
+
+/* Composition is an alternative to inheritance, where you can use the behavior of an 
+object without the dependency caused by direct class/subclass relationship.
+To use object composition, you need to store the object that has the desired behavior 
+as a member variable for the containing object. This is the basic strategy, which can be 
+implemented in at least three ways in C++:
+• Storing a pointer to an object: In this case, only a pointer to the 
+object is stored as part of the class. This option allows an object to be 
+created inside the class or passed as a parameter from a user of the 
+class and then stored in a member variable.
+
+• Storing a reference to an object: This option allows the class to receive 
+a reference to an existing object, but doesn’t allow the object to be 
+created after the constructor is executed. A reference in C++ cannot 
+be reassigned, which leads to a requirement that the stored object 
+needs to be valid the whole time the container object exists.
+• Storing the object directly as a member variable: In this case, the 
+containing class assumes complete responsibility for storing the 
+required object. Here, it is also necessary for the compiler to know 
+the exact size and type of the object stored as a member variable, 
+which reduces the flexibility of this method.
+With object composition, a class can use the functionality provided by another class 
+without the use of inheritance*/
+
+// Design patterns for options processing
+// Factory: Instead of asking clients to perform the initialization steps, factory methods provide a simple interface that can be called to create the objectand return a reference.
+// Singleton: a class that can have at most one active instance
+// Observer: allows objects to receive notifications for important events occurring in the system. This pattern also reduces the coupling between objects in the system, since the generator of notification events doesn’t need to know the details of the observers
+// Visitor: The visitor pattern allows a member function of an object to be called in response to another dynamic invocation implemented in a separate class.
+
+
+// Factories are used to indirectly create objects of a particular class
+/* , using the factory method design pattern, it is possible to avoid the use of the 
+new keyword to create an object, along with the parameters required by the constructor.
+The factory design pattern allows an object to be created through a member function 
+of the desired class, so that the client doesn’t need to create the object directly. */
+// Factory methods in c++ are implemented as static member functions
+
+/* A 
+situation where the use of a factory method is desirable is when you need to load data 
+objects. The data source used can vary from a local file to a URL, and the parsing of 
+that data is not an important part of the overall algorithm. In that case, abstracting the 
+creation of the data source can be an important application of the factory method */
+
+// When using factory methods, it is frequently useful to hide the real implementation of the constructor.This can be done through careful use of the private modifier.
+
+class DataSource {
+private:
+	DataSource(const std::string& name);  // private constructors
+	DataSource(const DataSource& p);
+	DataSource& operator= (const DataSource& p);
+public:
+	~DataSource();  // must be public so clients can use delete
+	static DataSource* createInstance();  // static function, public, used to generate an instance
+	void readData();
+private:
+	std::string m_dataName;
+};
+
+
+DataSource::DataSource(const std::string& name) : m_dataName(name)
+{
+
+}
+
+DataSource::DataSource(const DataSource& p) : m_dataName(p.m_dataName)
+{
+
+}
+
+DataSource& DataSource::operator= (const DataSource& p)
+{
+	if (this != &p)
+	{
+		m_dataName = p.m_dataName;
+	}
+	return *this;
+}
+
+DataSource::~DataSource()
+{
+}
+
+DataSource * DataSource::createInstance()
+{
+	std::string sourceName;
+	// Complex method used here to find sourceName and other construction 
+		DataSource* ds = new DataSource(sourceName);
+	return ds;
+}
+
+void DataSource::readData()
+{
+	// Read data here...
+}
+
+// Not member function!
+void useDataSource()
+{
+	// DataSource *source = new DataSource(""); // this will not work!
+	DataSource* source = DataSource::createInstance();
+	source->readData();
+	// Do something else with data
+	delete source;
+}
+
+// singleton pattern, a single object is used to represent the whole class, so there is a central location where services managed by that class can be redirected
+// clearing house can be represented as a singleton
+/*  A clearing house is an institution that provides clearing services for
+trades on options and derivatives.The clearing house makes sure that every trade has
+collateral so that counterpart risk is reduced, among other attributions.For example, if a
+trader sells options in a particular instrument, the clearing house will make sure that the
+trader has enough margin to satisfy the requirements of that particular trade.
+While a clearing house provides important services in the trading industry, most
+applications need to connect to a single clearing house.Thus, creating a single object to
+represent the clearing house is an obvious implementation technique for this situation.  */
+
+class clearingHouse {
+private:
+	clearingHouse();  // constructors, destructor, assignment operator all made private
+	clearingHouse(const clearingHouse& p);
+	clearingHouse& operator=(const clearingHouse& p);
+	~clearingHouse();
+
+public:
+	static clearingHouse& getClearingHouse();
+	void clearTrade(const Trade& t);
+
+private:
+	static clearingHouse* s_clearingHouse;  // the private member is a pointer to the only instance
+};
+
+clearingHouse* clearingHouse::s_clearingHouse = nullptr;  // set this to null initially
+
+clearingHouse::clearingHouse() // private constructor, cannot be used by clients
+{
+}
+clearingHouse::~clearingHouse() // this is private and cannot be used by clients
+{
+}
+
+clearingHouse& clearingHouse::getClearingHouse()
+{
+	if (!s_clearingHouse)
+	{
+		s_clearingHouse = new clearingHouse();
+	}
+	return *s_clearingHouse;
+}
+void clearingHouse::clearTrade(const Trade& t)
+{
+	// Trade is processed here
+}
+void useClearingHouse()
+{
+	Trade trade;
+	clearingHouse& ch = clearingHouse::getClearingHouse();
+	ch.clearTrade(trade);
+}
+
+// observer design pattern
+// observer design pattern is a powerful strategy to manage event updates by giving clients the ability to listen to events and updates to a particular object and react accordingly
+// observing objects can have pointers to them stored in the object that is creating events
+// the notify function can be called after the event function activates, and each observer object can call thier notify() function
+
+// visitor pattern
+//
+
+
+
+
+
 
 int main() 
 {
