c15f4eaf96fbaae05d03d2d1b31a2158b720476c
AHussain98 <106779657+AHussain98@users.noreply.github.com>
2024-08-01 10:50:14 +0100

    Update Basics.cpp

--- Books/Basics.cpp
+++ Books/Basics.cpp
@@ -164,6 +164,38 @@ For instance, if an initialization method needs to be called right after the con
 The reason for making the constructor protected, rather than private, is the same as for making any other method or field protected instead of private: so that it can be inherited by children. Perhaps you want a public, non-virtual factory method in the base class, which returns references to instances of the derived classes; 
 the derived classes obviously want access to the parent constructors, but you still don't want to be creating them outside of your factory.
 
+
+CPP Compilation and Linking 
+
+
+The compilation is the process which convert the program written in human readable language like C, C++ etc into a machine code, directly understood by the Central Processing Unit. 
+
+
+Preprocessing
+
+In this phase the preprocessor changes the progrPreprocessingam according to the directives mentioned (that starts with # sign). 
+The C++ preprocessor takes the program and deals with the # include directives and the resulting program is pure c++ program. 
+For example, in c++ program #include<iostream> will tell the preprocessor to read all the contents of the iostream header file and include the contents into the program and generate the separate C++ program file. 
+C++ supports many preprocessor directives like #include, #define, #if, #else etc
+
+Compilation
+
+This phase translates the program into a low level assembly level code. 
+The compiler takes the preprocessed file ( without any directives) and generates an object file containing assembly level code. 
+Now, the object file created is in the binary form. In the object file created, each line describes one low level machine level instruction.
+The created object files can be placed in special archives which we refer to as static libraries, for easier reusing in the future.
+But you still can’t run these object files until you convert them into executable file, now here linker comes into play, which links all the object files to generate single executable file. 
+
+Linking
+
+Linking as the name suggests, refers to creation of a single executable file from multiple object files. 
+The file created after linking is ready to be loaded into memory and executed by the system. 
+There is difference in linking and compilation when it comes to understanding errors. 
+Compiler shows errors in syntax, for example semi-colon not mentioned, data type not defined etc but if there is an error that function has been defined multiple times, then this error is from linker as its indicating that two or more source code files have the same meaning and that is leading to an error.
+
+
+
+
 */
 
 
